3746fb22ed41d183b5fc0ded9bf73ff28e561c52
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-20 05:03:17 +0000

    day 4

---
+++ main.py
@@ -0,0 +1,17 @@
+# RANDOM MODULE
+# import random
+
+# random_integer = random.randint(1,10)
+# random_float = random.random()*5
+# print(random_float)
+
+
+#LIST DATA STRUCTURE
+# states_of_america = ["Delaware", "California"]
+
+fruits = ["Strawberries","Apples","Grapes","Peaches","Cherries","Pears","Nectarines"]
+
+vegetables = ["Spinach","Kale","Tomatoes","Celery","Potatoes"]
+
+dirty_dozen = [fruits,vegetables]
+print(dirty_dozen)
